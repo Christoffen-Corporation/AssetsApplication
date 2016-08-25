a8f8b003a0c573f2ea5b3da34cd15a22b050cbae
M. Gage Morgan <gagemorgan@outlook.com>
2016-08-24 20:57:53 -0400

    Added ability to copy needed UI files from location to bin when needed

--- src/Assets/Main.c
+++ src/Assets/Main.c
@@ -2,87 +2,11 @@
 #include <sys/stat.h>
 #include <argp.h>
 #include <gtk/gtk.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 #include "../API/include.h"
-
-void onChatAllPiecesClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/IconChat/All", &st) == -1) {
-    mkdir("AppContent/IconChat/All", 0700);
-}
-
-ChatIconFull();
-}
-
-void onChatJustPiecesClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/IconChat/PiecesOnly", &st) == -1) {
-    mkdir("AppContent/IconChat/PiecesOnly", 0700);
-}
-
-ChatIconPiecesOnly();
-}
-
-void onFrankenlogosClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/Frankenlogos", &st) == -1) {
-    mkdir("AppContent/Logo/Frankenlogos", 0700);
-}
-
- Frankenlogos();
-}
-
-void onColoredNoLogoClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/ColoredNoLogo", &st) == -1) {
-    mkdir("AppContent/Logo/ColoredNoLogo", 0700);
-}
-
- ColoredNoLogo(); 
-}
-
-void onAllClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/All", &st) == -1) {
-    mkdir("AppContent/Logo/All", 0700);
-}
-
- AllImages();
-}
-
-void onOutlinedNoLogoClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/OutlinedNoLogo", &st) == -1) {
-    mkdir("AppContent/Logo/OutlinedNoLogo", 0700);
-}
-
- OutlinedNoLogo();
-}
-
-void onOutlinedAllClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/OutlinedAll", &st) == -1) {
-    mkdir("AppContent/Logo/OutlinedAll", 0700);
-}
-
- OutlinedAll();
-}
-
-void onAllColorsClicked() {
-struct stat st = {0};
-
-if (stat("AppContent/Logo/AllColors", &st) == -1) {
-    mkdir("AppContent/Logo/AllColors", 0700);
-}
-
- ColoredAll();
-}
 
 const char *argp_program_version = "Christoffen Assets";
 const char *argp_program_bug_address = "M. Gage Morgan <gage@christoffen.com>";
@@ -109,9 +33,14 @@ static int parse_opt (int key, char *arg, struct argp_state *state) {
  return 0;
 }
 
-
 int main(int argc, char *argv[]) {
 struct stat st = {0};
+
+if (stat("UI", &st) == -1) {
+    mkdir("UI", 0700);
+}
+
+CopyFiles();
 
 if (stat("AppContent", &st) == -1) {
     mkdir("AppContent", 0700);
@@ -132,7 +61,7 @@ if (argc == 1) {
     gtk_init(&argc, &argv);
  
     builder = gtk_builder_new();
-    gtk_builder_add_from_file (builder, "../src/API/UI/MainWindow.ui", NULL);
+    gtk_builder_add_from_file (builder, "UI/MainWindow.ui", NULL);
  
     window = GTK_WIDGET(gtk_builder_get_object(builder, "MainWindow"));
     gtk_builder_connect_signals(builder, NULL);
